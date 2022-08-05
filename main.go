9ad9af2e48eacddffdfd7b1da01373f64ad430fa
lamhai1401 <lamthanhhai141@gmail.com>
2022-08-05 20:22:20 +0700

    recipe data model

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 	router.Run()
+}
+
+// Recipe linter
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
